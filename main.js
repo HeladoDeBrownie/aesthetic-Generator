1425a6397101f7348b0f6d2911b43d27e8a2fd25
helado de brownie <helado.de.brownie@gmail.com>
2019-11-28 01:51:24 -0500

    add regenerate button and functionality

--- main.js
+++ main.js
@@ -1,18 +1,26 @@
 void function () {
     'use strict'
 
+    var resultElement = document.getElementById('generated-aesthetic')
+
     addEventListener('error', function () {
         resultElement.textContent = 'Oops, something broke.'
     })
 
-    var resultElement = document.getElementById('generated-aesthetic')
+    function regenerate(grammar) {
+        document.getElementById('generated-aesthetic').textContent = grammar.flatten('#origin#')
+    }
 
     var request = new XMLHttpRequest()
     request.open('GET', 'grammar.json', true)
     request.responseType = 'json'
     request.addEventListener('load', function () {
         if (request.status === 200) {
-            document.getElementById('generated-aesthetic').textContent = tracery.createGrammar(request.response).flatten('#origin#')
+            var grammar = tracery.createGrammar(request.response)
+            regenerate(grammar)
+            var regenerateElement = document.getElementById('regenerate')
+            regenerateElement.addEventListener('click', regenerate.bind(null, grammar))
+            regenerateElement.disabled = ''
         } else {
             throw Error("The grammar couldn't be fetched.")
         }
