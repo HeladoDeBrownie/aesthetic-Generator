ffa14013cdda33877d9a3ed7a8e4e1d460f8857c
helado de brownie <helado.de.brownie@gmail.com>
2019-11-30 01:42:56 -0500

    rework, reorganize, refactor

--- main.js
+++ main.js
@@ -1,33 +1,35 @@
 void function () {
     'use strict'
 
-    var resultElement = document.getElementById('generated-aesthetic')
+    var $ = document.getElementById.bind(document)
 
-    addEventListener('error', function () {
-        resultElement.textContent = 'Oops, something broke.'
-    })
-
-    function regenerate(grammar) {
-        var aestheticName = grammar.flatten('#origin#')
-        document.getElementById('generated-aesthetic').textContent = aestheticName
-        var tweetThisElement = document.getElementById('tweet-this')
-        tweetThisElement.href = 'https://twitter.com/intent/tweet?text=' + encodeURIComponent(document.getElementsByClassName('aesthetic-description')[0].textContent + '\n#aestheticGenerator https://helado.itch.io/aesthetic-generator')
-        tweetThisElement.hidden = ''
+    function show(result) {
+        $('result').textContent = result
+        $('tweet').href = 'https://twitter.com/intent/tweet?text=' + encodeURIComponent($('content').textContent + '\n#aestheticGenerator https://helado.itch.io/aesthetic-generator')
     }
 
-    var request = new XMLHttpRequest()
-    request.open('GET', 'grammar.json', true)
-    request.responseType = 'json'
+    addEventListener('error', show.bind(null, 'Oops, something broke.'))
+
+    var request = new XMLHttpRequest
+
     request.addEventListener('load', function () {
-        if (request.status === 200) {
+        if (request.status === 200 && request.response instanceof Object) {
             var grammar = tracery.createGrammar(request.response)
-            regenerate(grammar)
-            var regenerateElement = document.getElementById('regenerate')
-            regenerateElement.addEventListener('click', regenerate.bind(null, grammar))
-            regenerateElement.hidden = ''
+            var rerollElement = $('reroll')
+
+            rerollElement.addEventListener('click', function () {
+                show(grammar.flatten('#origin#'))
+            })
+
+            rerollElement.click()
+            rerollElement.parentElement.hidden = false
+            $('tweet').parentElement.hidden = false
         } else {
-            throw Error("The grammar couldn't be fetched.")
+            throw Error("Something went wrong fetching the grammar.")
         }
     })
+
+    request.open('GET', 'grammar.json')
+    request.responseType = 'json'
     request.send()
 }()
